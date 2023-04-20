47058c2be7ce56f19f91f03a54640a2a3d468e52
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-04-20 11:28:57 +0700

    feat : add new chat control flow

--- src/frontend/src/components/MenuButton.js
+++ src/frontend/src/components/MenuButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function MenuButton(props) {
     return (
-        <button onClick={props.onClick} class="mr-5 py-3 text-light hover:font-bold focus:font-bold focus:bg-gray-700 hover:bg-gray-700 rounded-md my-2">
+        <button onClick={props.onClick} class="mr-5 py-3 text-light hover:font-bold focus:font-bold focus:bg-gray-700 hover:bg-gray-700 rounded-md my-1">
             {props.label}
         </button>
     );
