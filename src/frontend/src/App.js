55fbc3998b3526cbfca2b1a2fa498fff702cfb89
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-04-22 11:58:26 +0700

    feat : history chat run properly, to-do : page size handle

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import React from "react";
 import ChatBot from "./pages/ChatBot";
-import Coba from "./pages/Coba";
 
 function App() {
     return (
@@ -12,7 +11,6 @@ function App() {
                 <Routes>
                     <Route path="/" element={<ChatBot />} />
                     <Route path="*" element={<ChatBot />} />
-                    <Route path="/coba" element={<Coba />} />
                 </Routes>
             </BrowserRouter>
         </div>
