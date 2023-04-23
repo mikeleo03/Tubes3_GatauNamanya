b081f44698ba0320f2239103c665fb107d16163b
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-04-24 00:54:08 +0700

    feat : responsive design (pt. 3)

--- src/frontend/src/components/Additional.js
+++ src/frontend/src/components/Additional.js
@@ -11,10 +11,10 @@ function MenuButton(props) {
 function Profile(props) {
     return (
         <button class="profile-section" className="h-18 flex flex-row space-x-1 mb-4 bg-gray-600 focus:bg-gray-500 hover:bg-gray-500 rounded-md px-3 w-full">
-            <div className="w-1/5 flex justify-center align-center py-2">
+            <div className="w-1/5 flex justify-center align-center py-2 shrink-0">
                 <img src={props.profpics} alt="Profile" className="h-10"></img>
             </div>
-            <div className="w-4/5 pl-2 py-2 align-left">
+            <div className="w-4/5 pl-2 py-2 align-left overflow-hidden">
                 <p className="text-light text-left">{props.name}</p>
                 <p className="text-light text-xs text-elipsis text-left">{props.email}</p>
             </div>
--- src/frontend/src/components/ChatHeader.js
+++ src/frontend/src/components/ChatHeader.js
@@ -8,10 +8,10 @@ const ChatHeader = ({ pages, setPages, currentPage, openHistory, setOpenHistory 
     };
 
     return (
-        <div className="h-14 py-3 w-full flex space-x-2">
+        <div className="md:h-14 h-12 py-3 w-full flex space-x-2">
             <div className='flex-item w-full align-left'>
                 <input
-                    class="align-left font-medium text-lg focus:outline-none w-full pr-5 md:pr-3"
+                    class="align-left font-medium md:text-lg focus:outline-none w-full pr-5 md:pr-3"
                     type="text"
                     value={ (pages[currentPage] && pages[currentPage].name) ? (pages[currentPage].name) : ("") }
                     onChange={(event) => handleRenamePage(currentPage, event.target.value)}
@@ -22,18 +22,18 @@ const ChatHeader = ({ pages, setPages, currentPage, openHistory, setOpenHistory 
                 <button
                     onClick={() => setOpenHistory(!openHistory)}
                     type="button"
-                    className="bg-gray-700 inline-flex items-center justify-center p-2 py-1 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                    className="bg-gray-700 inline-flex items-center justify-center md:p-2 p-1 md:py-1 py-0.5 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                     aria-controls="mobile-menu"
                     aria-expanded="false"
                 >
                 {!openHistory ? (
                     <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
+                        className="block h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
                     >
                     <path
                         strokeLinecap="round"
@@ -44,12 +44,12 @@ const ChatHeader = ({ pages, setPages, currentPage, openHistory, setOpenHistory 
                     </svg>
                 ) : (
                     <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
+                        className="block h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
                     >
                     <path
                         strokeLinecap="round"
