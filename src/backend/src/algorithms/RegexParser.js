4929a7ec29c875fed2c56941a2848df405d303fd
Nat10k <110515021+Nat10k@users.noreply.github.com>
2023-05-01 07:26:39 +0700

    feat : handle multiple questions with ; separator

--- src/backend/src/algorithms/RegexParser.js
+++ src/backend/src/algorithms/RegexParser.js
@@ -5,35 +5,42 @@ import Calculate from "./Calculate.js";
 const days = ["Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu"];
 
 async function getAnswer(text, algorithm) {
-    let type = Regex(text);
-    let q,a = "";
-    switch (type) {
-        case "date" :
-            let split_date = text.replace(/\s*((hari)?\s*(apa)?)?\s*/i, "").split("/");
-            split_date[2] = split_date[2].substring(0,4);
-            let d = new Date(split_date[1] + "/" + split_date[0] + "/" + split_date[2]);
-            return "Hari " + days[d.getDay()];
-
-        case "calculator" :
-            return Calculate(text);
-
-        case "add" :
-            text = text.replace(/(tambahkan|tambah) pertanyaan /i, "").replace(/dengan jawaban /i, ",").trim().split(",");
-            q = text[0].toLowerCase().trim();
-            a = text[1].trim();
-            let res = await Database.save_question(q,a,algorithm);
-            return res;
-
-        case "delete" :
-            text = text.replace(/hapus pertanyaan /i, "").trim();
-            q = text.toLowerCase();
-            a = await Database.delete_question(q,algorithm);
-            return a;
-
-        default :
-            a = await Database.get_answer(text.toLowerCase(),algorithm);
-            return a;
+    let split_text = text.split(";");
+    let responses = [];
+    for (let t of split_text) {
+        t = t.trim();
+        let type = Regex(t);
+        let q,a = "";
+        switch (type) {
+            case "date" :
+                let split_date = t.replace(/\s*((hari)?\s*(apa)?)?\s*/i, "").split("/");
+                split_date[2] = split_date[2].substring(0,4);
+                let d = new Date(split_date[1] + "/" + split_date[0] + "/" + split_date[2]);
+                responses.push("Hari " + days[d.getDay()]);
+                break;
+            case "calculator" :
+                responses.push(Calculate(t));
+                break;
+            case "add" :
+                t = t.replace(/(tambahkan|tambah) pertanyaan /i, "").replace(/dengan jawaban /i, ",").trim().split(",");
+                q = t[0].toLowerCase().trim();
+                a = t[1].trim();
+                let res = await Database.save_question(q,a,algorithm);
+                responses.push(res);
+                break;
+            case "delete" :
+                t = t.replace(/hapus pertanyaan /i, "").trim();
+                q = t.toLowerCase();
+                a = await Database.delete_question(q,algorithm);
+                responses.push(a);
+                break;
+            default :
+                a = await Database.get_answer(t.toLowerCase(),algorithm);
+                responses.push(a);
+                break;
+        }
     }
+    return responses;
 }
 
 export default getAnswer;
