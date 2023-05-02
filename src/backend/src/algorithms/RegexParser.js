99c6ac51611f71b2c884c6e76ba30cbc1337a81f
Nat10k <110515021+Nat10k@users.noreply.github.com>
2023-05-02 08:39:08 +0700

    fix : add newline to the end of each answer

--- src/backend/src/algorithms/RegexParser.js
+++ src/backend/src/algorithms/RegexParser.js
@@ -7,8 +7,8 @@ const days = ["Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu"];
 async function getAnswer(text, algorithm) {
     let split_text = text.split(";");
     let responses = [];
-    for (let t of split_text) {
-        t = t.trim();
+    for (let i = 0; i < split_text.length; i++) {
+        let t = split_text[i].trim();
         let type = Regex(t);
         let q,a = "";
         switch (type) {
@@ -16,37 +16,39 @@ async function getAnswer(text, algorithm) {
                 let split_date = t.replace(/Hari/i, "").replace(/apa/i, "").replace("?","").split("/");
                 let d = new Date(split_date[1].trim() + "/" + split_date[0].trim() + "/" + split_date[2].trim());
                 if (days[d.getDay()] == undefined) {
-                    responses.push("Tanggal tidak valid");
+                    a = "Tanggal tidak valid";
                     break;
                 }
-                responses.push("Hari " + days[d.getDay()]);
+                a = "Hari " + days[d.getDay()];
                 break;
             case "calculator" :
                 t = t.replace("=", "").replace("?","");
-                responses.push(Calculate(t));
+                a = String(Calculate(t));
                 break;
             case "add" :
                 t = t.replace(/(tambahkan|tambah) pertanyaan /i, "").replace(/dengan jawaban /i, ",").trim().split(",");
                 q = t[0].toLowerCase().trim();
                 a = t[1].trim();
                 let res = await Database.save_question(q,a,algorithm);
-                responses.push(res);
+                a = res;
                 break;
             case "delete" :
                 t = t.replace(/hapus pertanyaan /i, "").trim();
                 q = t.toLowerCase();
                 a = await Database.delete_question(q,algorithm);
-                responses.push(a);
                 break;
             default :
                 if (t.trim() === "") {
-                    responses.push("Pertanyaan kosong");
+                    a = "Pertanyaan kosong";
                     break;
                 }
                 a = await Database.get_answer(t.toLowerCase(),algorithm);
-                responses.push(a);
                 break;
         }
+        if (i < split_text.length) {
+            a += "\n";
+        }
+        responses.push(a);
     }
     return responses;
 }
