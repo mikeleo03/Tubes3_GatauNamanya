9d06370e3d02c240cc85f71880f6d78c5da9ab41
Nat10k <110515021+Nat10k@users.noreply.github.com>
2023-05-03 09:06:26 +0700

    fix : booyer-moore algorithm

--- src/backend/src/algorithms/Booyer Moore.js
+++ src/backend/src/algorithms/Booyer Moore.js
@@ -1,35 +1,22 @@
-let char_last_idx = {}
-
 function fill_last_indexes(pattern) {
-    let idx = pattern.length-1;
+    let char_last_idx = {};
+    let idx = 0;
     for (let c of pattern) {
-        if (!(c in char_last_idx)) {
-            char_last_idx[c] = idx;
-        }
-        idx--;
+        char_last_idx[c] = idx;
+        idx++;
     }
-}
-
-function last_index(char) {
-    if (!(char in char_last_idx)) {
-        return -1;
-    } else {
-        return char_last_idx[char];
-    }
+    return char_last_idx;
 }
 
 function booyer_moore(text,pattern) {
-    // Reset last index dictionary
-    char_last_idx = {};
+    // Isi daftar indeks terakhir setiap karakter dalam pattern
+    let char_last_index = fill_last_indexes(pattern);
     let n = text.length;
     let m = pattern.length;
     let i = m-1;
     if (i > n-1) {
         return -1;
     }
-
-    // Isi daftar indeks terakhir setiap karakter dalam pattern
-    fill_last_indexes(pattern);
 
     let j = m-1;
     do {
@@ -41,7 +28,10 @@ function booyer_moore(text,pattern) {
                 j--;
             }
         } else {
-            let lo = last_index(text[i]);
+            let lo = -1;
+            if (text[i] in char_last_index) {
+                lo = char_last_index[text[i]];
+            }
             i = i + m - Math.min(j,lo+1);
             j = m-1;
         }
